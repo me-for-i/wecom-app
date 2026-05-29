8cbce4b8e89e08a9ce026568ea0743715dc01dc7
尹得荀 <dexun.yin@miotech.com>
2026-05-29 18:25:56 +0800

    读取消息

---
+++ src/main/java/org/example/wecomapp/WecomAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.wecomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WecomAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WecomAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/wecomapp/WecomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wecomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WecomAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
